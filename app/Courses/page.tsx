6c81b584919b63cc40c8a3b3eaefff657255abc2
shoaib memon <shoaibattari83@gmail.com>
2023-03-01 16:50:31 +0500

    edit

--- app/Courses/page.tsx
+++ app/Courses/page.tsx
@@ -5,9 +5,9 @@ import { cards } from "../Courses/Database";
 function page() {
   return (
     <div>
-      <div className=" px-24 sm:py-32">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto max-w-2xl lg:text-center">
+      <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:text-center">
             <p className="mt-2 text-5xl font-bold tracking-tight text-gray-900 sm:text-4xl">
               All Courses
             </p>
@@ -19,8 +19,8 @@ function page() {
           <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
             <dl className="grid max-w-lg grid-cols-1 gap-y-10 gap-x-8 lg:max-w-none lg:grid-cols-2 lg:gap-y-16 ">
               {cards.map((ele) => (
-                <div key={ele.id} className="pl-16 ">
-                  <dt className="font-bold text-3xl text-indigo-600">
+                <div key={ele.id} className="pl-2 mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl ">
+                  <dt className="font-bold text-3xl text-indigo-600 ">
                     {ele.heading1}
                   </dt>
                   <dd className="mt-2 text-base leading-10 text-gray-600">
